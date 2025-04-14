ea93b757f9d5d90f18da082e9713bf044744557f
nouman <muhammadusman@gsoftcosulting.com>
2025-04-14 22:15:09 +0500

    stripe

--- src/config/fe/stripe.ts
+++ src/config/fe/stripe.ts
@@ -1,14 +1,13 @@
 function getStripePublishablesKey(): string {
-  const publishablesKey =
-    process.env.NODE_ENV === "production"
-      ? process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_LIVE
-      : process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_TEST;
-  if (publishablesKey === undefined) {
-    throw new Error(
-      "Missing NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_TEST or NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_TEST in .env file"
-    );
-  }
-  return publishablesKey;
+  const publishablesKey = process.env.NODE_ENV === "production";
+  // ? process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_LIVE
+  // : process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_TEST;
+  // if (publishablesKey === undefined) {
+  //   throw new Error(
+  //     "Missing NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_TEST or NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_TEST in .env file"
+  //   );
+  // }
+  return "publishablesKey";
 }
 
 function getStripePlusPriceId(): string {
